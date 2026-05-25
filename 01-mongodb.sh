485689f343d6d66087b42fca48778c947199bd60
Vamsi Krishna P <vamsikrishnap@hotmail.com>
2026-05-25 13:40:52 +0530

    hello world

--- 01-mongodb.sh
+++ 01-mongodb.sh
@@ -19,7 +19,7 @@ if [ $USERID -ne 0 ]; then
 fi
 
 VALIDATE(){
-    if [ $ -ne 0]; then
+    if [ $1 -ne 0]; then
         echo -e "$TIMESTAMP [ERROR] $2 ... $R FAILURE $N" | tee -a $LOGS_FILE
         exit 1
     else
