c85016c38d0a1da9935bff45b5692afe1879146c
Vamsi Krishna P <vamsikrishnap@hotmail.com>
2026-05-27 16:47:20 +0530

    hello world

--- roboshop-v2.sh
+++ roboshop-v2.sh
@@ -45,7 +45,7 @@ do
             --query 'Instances[0].InstanceId' \
             --output text 
             )
-            echo "Launched Instance: #INSTANCE_ID"
+            echo "Launched Instance: $INSTANCE_ID"
         else
             echo "roboshop-$instance already running: $INSTANCE_ID"
         fi
